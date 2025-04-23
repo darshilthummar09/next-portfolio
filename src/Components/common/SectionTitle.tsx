da90675e3339786eb42b1f823845f180b4d82711
darshil thummar <darshilthummar952002@gmail.com>
2025-04-23 20:53:18 +0530

    Add favicon images and web manifest for the application
    - Added favicon-16x16.png and favicon-32x32.png images to enhance the app's branding.
    - Created site.webmanifest file to define app metadata and icons for better integration with mobile devices.

--- src/Components/common/SectionTitle.tsx
+++ src/Components/common/SectionTitle.tsx
@@ -1,6 +1,10 @@
-const SectionTitle = ({ children }: Readonly<{ children: string }>) => {
-  return (
-    <p className="text-center mx-auto text-3xl/6 md:text-4xl/6 font-bold">
+type SectionTitleProps = {
+  children: string;
+  id?: string;
+};
+
+const SectionTitle = ({ children, id }: SectionTitleProps) => {  return (
+    <p className="text-center mx-auto text-3xl/6 md:text-4xl/6 font-bold p-5">
       {children}
     </p>
   );
