f19078c4fe258a4d9309ad4d754224636a2b126c
Darshil Thummar <78547530+darshilthummar09@users.noreply.github.com>
2026-03-14 10:32:34 +0530

    Include SDE role at Codewinglet with project details
    Added current position as Software Development Engineer at Codewinglet, detailing responsibilities and technologies used.

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -11,6 +11,16 @@ const experience: IExperienceItem[] = [
     companyLogo: "/experience/sparkle-logo.png",
     companyLink:"https://sparkleinfotech.com/"
   },
+  {
+    company: "Codewinglet Private Limited",
+    position: "Software Development Engineer (SDE)",
+    startDate: "Feb 2026",
+    endDate: "present",
+    description: "Currently working as a Software Development Engineer (SDE) at Codewinglet Pvt. Ltd., contributing to the development of scalable web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js). I collaborate with cross-functional teams to design, develop, and deploy high-performance applications while following agile development practices. I also work with modern technologies such as Next.js, REST APIs, cloud services, and AI-driven solutions to build secure and scalable products.",
+    location: "Surat, Gujarat, India",
+    companyLogo: "/experience/cw-logo.svg",
+    companyLink:"https://www.codewinglet.com/"
+  },
 ];
 
 export default experience;
